95962636ddfe43e4e94c549cdc94774de425cacb
Rahuljain16 <rj267043@gmail.com>
2025-06-13 00:04:10 +0530

    latestone

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const { faker } = require('@faker-js/faker');
 const mysql2 = require('mysql2')
 const express = require("express");
 const app = express();
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views",path.join(__dirname,"/views"));
 
 const connection =  mysql2.createConnection({
   host: 'localhost',
@@ -28,28 +32,40 @@ for(let i=1; i<100; i++){
 */  
 
 
-
+//HOME PAGE
 app.get("/",(req,res) => {
   let q = `SELECT count(*) FROM user`;
-  
-
-try{
+  try{
   connection.query(q, (error,result) =>{
     if(error) throw error;
-    console.log(result[0]["count(*)"]);
-    res.send("success");
+    let count = result[0]["count(*)"];
+    res.render("home.ejs",{count});
   })
 } catch(error) {
   console.log(error);
   res.send("some error in DB");
 }
-connection.end();
 })
+
+//SHOW ROUTE
+app.get("/user",(req,res) => {
+  let q = `SELECT * FROM user`;
+  try{
+  connection.query(q, (error,result) =>{
+    if(error) throw error;
+   // console.log(result);
+    res.render("showusers.ejs");
+  })
+} catch(error) {
+  console.log(error);
+  res.send("some error in DB");
+}
+});
 
 
 app.listen("8080",() => {
 console.log("app is listening");
-})
+});
 
 
 
