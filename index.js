9ab1bbac372e71614ef27bd1294eab75e2112f09
Rahuljain16 <rj267043@gmail.com>
2025-06-12 15:11:12 +0530

    latest

--- index.js
+++ index.js
@@ -9,10 +9,12 @@ const connection =  mysql2.createConnection({
 });
 
 let q = "INSERT INTO user (id, username,email,password) VALUES ?";
-let users = [["123b","abc_newuserb","123@gmail.comb","abcb"],
-            ["123c","abc_newuserc","123@gmail.comc","abcc"]];
+let data = [];
+for(let i=1; i<100; i++){
+  getRandomUser();
+}
 
-try{
+/*try{
   connection.query(q,[users], (error,result) =>{
     if(error) throw error;
     console.log(result);
@@ -21,14 +23,14 @@ try{
   console.log(error)
 }
 connection.end();
-
+*/
 
 let getRandomUser= () => {
-  return {
-    id: faker.string.uuid(),
-    username: faker.internet.username(),
-    email: faker.internet.email(),
-    password: faker.internet.password()
-  };
+  return [
+     faker.string.uuid(),
+     faker.internet.username(),
+     faker.internet.email(),
+     faker.internet.password()
+  ];
 }
 
